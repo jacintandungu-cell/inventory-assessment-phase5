f899168b7f8dac59cb0603a7b1109c2c2cc0848e
jacintandungu <jacintandungu@users.noreply.github.com>
2026-07-05 01:06:41 +0300

    add GET routes to view all inventory items and one item by id

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify
 
+from data import inventory
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return jsonify({'message': 'Welcome to the inventory management API'})
+
+@app.route('/inventory', methods=['GET'])
+def get_inventory():
+    return jsonify({'inventory': inventory}), 200
+
+@app.route('/inventory/<int:item_id>', methods=['GET'])
+def get_inventory_item(item_id):
+    for item in inventory:
+        if item['id'] == item_id:
+            return jsonify(item), 200
+    return jsonify({'error': 'Item not found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
