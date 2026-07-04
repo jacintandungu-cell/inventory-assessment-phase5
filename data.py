3982829f1d0eb916ff8c969f354aafb9298d3262
jacintandungu <jacintandungu@users.noreply.github.com>
2026-07-05 01:03:02 +0300

    create mock inventory database as a python list

---
+++ data.py
@@ -0,0 +1,18 @@
+inventory = [
+    {
+        'id': 1,
+        'name': 'Organic Almond Milk',
+        'brand': 'Silk',
+        'barcode': '1234567890123',
+        'price': 3.99,
+        'quantity': 12,
+    },
+    {
+        'id': 2,
+        'name': 'Whole Wheat Bread',
+        'brand': "Nature's Own",
+        'barcode': '9876543210987',
+        'price': 2.49,
+        'quantity': 20,
+    },
+]
